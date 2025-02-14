734087780daf5b244169dfdb55800394e2db76ad
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-14 10:17:15 -0300

    feat: add project list component

--- src/app/yield/page.tsx
+++ src/app/yield/page.tsx
@@ -1,9 +1,13 @@
-import { TopSection } from "@/components/top-section";
+import { ProjectList } from "@/components/yield-page-components/project-list";
+import { TopSection } from "@/components/yield-page-components/top-section";
 
 export default function YieldPage() {
+  const totalYield = 400000.45;
+
   return (
-    <div className="md:my-20 my-4">
+    <div className="md:my-20 my-4 space-y-16">
       <TopSection />
+      <ProjectList totalYield={totalYield} />
     </div>
   );
 }
