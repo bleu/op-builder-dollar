262e9332565a7da067c19b9b0e688a12763bbd40
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-17 09:20:16 -0300

    feat: add cohort selection page with top section

--- src/app/cohort-selection/page.tsx
+++ src/app/cohort-selection/page.tsx
@@ -1,7 +1,11 @@
+import { TopSection } from "@/components/yield-page-components/top-section";
+
 export default function CohortSelectionPage() {
+  const totalYield = 400000.45;
+
   return (
-    <div>
-      <h1>Cohort Selection</h1>
+    <div className="my-20 md:pt-20 pt-4 space-y-16 max-w-[760px] mx-auto px-4">
+      <TopSection totalYield={totalYield} />
     </div>
   );
 }
