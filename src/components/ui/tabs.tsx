8b0619e09d7b68e77a09d47bd8ce2f3d7b00b0c8
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-18 08:42:42 -0300

    fix: remove shadow when tab active

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center text-xl text-sub-text border-primary/10 border bg-content whitespace-nowrap rounded-sm px-3 py-1.5 font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary/10 data-[state=active]:text-primary data-[state=active]:shadow-md data-[state=active]:border-none",
+      "inline-flex items-center justify-center text-xl text-sub-text border-primary/10 border bg-content whitespace-nowrap rounded-sm px-3 py-1.5 font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary/10 data-[state=active]:text-primary data-[state=active]:border-none",
       className,
     )}
     {...props}
