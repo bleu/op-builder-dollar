5a456ebd081bb8a3b902bf21aa6779120d1b5d8d
Jean Neiverth <jeanneiverth@gmail.com>
2025-02-26 13:59:08 -0300

    refactor: use op mainnet tests

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,16 @@
 import type { Address } from "viem";
 
+//Sepolia
+// export const USDC_ADDRESS =
+//   "0xbe72E441BF55620febc26715db68d3494213D8Cb" as Address;
+// export const OBUSD_ADDRESS =
+//   "0x70F9667013645EcC52f6ff98b0C60b37D7647e26" as Address;
+// export const BUILDERS_MANAGER_ADDRESS = "" as Address;
+
+// OP mainnet
 export const USDC_ADDRESS =
-  "0xbe72E441BF55620febc26715db68d3494213D8Cb" as Address;
+  "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1" as Address;
 export const OBUSD_ADDRESS =
-  "0x70F9667013645EcC52f6ff98b0C60b37D7647e26" as Address;
-export const BUILDERS_MANAGER_ADDRESS = "" as Address;
+  "0x51926Dfd0050a4842cE3Eac9Ffc8194e1bBE1297" as Address;
+export const BUILDERS_MANAGER_ADDRESS =
+  "0x1c87739c9cfb4a562817d4a1c2d62787a949eb11" as Address;
