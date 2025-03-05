6d191292f0d940f44fb19f9a3252a6e503cc96e0
Jean Neiverth <jeanneiverth@gmail.com>
2025-03-05 08:48:33 -0300

    fix: html body error

--- src/app/Content.tsx
+++ src/app/Content.tsx
@@ -3,13 +3,8 @@
 import { Footer } from "@/components/footer";
 import Header from "@/components/header";
 import { Providers } from "@/providers";
-import { Rubik } from "next/font/google";
-import { type ReactNode, useEffect, useState } from "react";
 
-const rubik = Rubik({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-});
+import { type ReactNode, useEffect, useState } from "react";
 
 export default function Content({ children }: { children: ReactNode }) {
   const [isMounted, setIsMounted] = useState(false);
@@ -22,12 +17,10 @@ export default function Content({ children }: { children: ReactNode }) {
     return null;
   }
   return (
-    <body className={`${rubik.className} bg-background text-foreground`}>
-      <Providers>
-        <Header />
-        {children}
-        <Footer />
-      </Providers>
-    </body>
+    <Providers>
+      <Header />
+      {children}
+      <Footer />
+    </Providers>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Rubik } from "next/font/google";
 import Content from "./Content";
 
 export const metadata: Metadata = {
   title: "OP Builders Dollar",
   description: "Optimism Builders Dollar",
 };
+
+const rubik = Rubik({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -17,8 +23,9 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/icons/logo.svg" type="image/png" />
       </head>
-
-      <Content>{children}</Content>
+      <body className={`${rubik.className} bg-background text-foreground`}>
+        <Content>{children}</Content>
+      </body>
     </html>
   );
 }
