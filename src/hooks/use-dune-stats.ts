bb105a6b11402d1a8cc490086f3fcaf5fedd006e
Jean Neiverth <jeanneiverth@gmail.com>
2025-03-10 11:45:14 -0300

    fix: total_transfer typo

--- src/hooks/use-dune-stats.ts
+++ src/hooks/use-dune-stats.ts
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useReadObusd } from "./use-read-obusd";
 
 interface DuneStats {
-  yield_transfers_count: number;
+  transfers_count: number;
   total_yield_generated: string;
   unique_recipients: number;
   unique_token_holders: number;
@@ -30,7 +30,7 @@ export const useDuneStats = () => {
 
       return {
         totalYieldGeneratedOvertime,
-        totalObusdTransactions: addCommasToInt(stats.yield_transfers_count),
+        totalObusdTransactions: addCommasToInt(stats.transfers_count),
         totalTeamsFunded: addCommasToInt(stats.unique_recipients),
         totalUsers: addCommasToInt(stats.unique_token_holders),
       };
